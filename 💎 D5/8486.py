121c5910b6f83947cb8a778d0f42ff6b5d14bddf
jyh071116 <jyh071116@gmail.com>
2024-02-14 07:12:58 +0900

    💎 D5-8486

---
+++ 💎 D5/8486.py
@@ -0,0 +1,27 @@
+k = int(input())
+n = 10**11
+
+mu = [0]*(int(n**0.5)+1)
+mu[1] = 1
+for i in range(1, int(n**0.5)+1):
+  for j in range(2*i, int(n**0.5)+1, i):
+    mu[j] -= mu[i]
+
+def sfi_num(n):
+  cnt = 0
+  i = 2
+  while i*i<=n:
+    cnt += mu[i]*(n//(i*i))
+    i += 1
+  return -cnt
+    
+    
+start, end = 0, n
+while start <= end:
+  mid = (start + end) // 2
+  if sfi_num(mid) >= k:
+    end = mid - 1
+  else:
+    start = mid + 1
+
+print(start)
