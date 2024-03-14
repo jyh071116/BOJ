d5f502e7e5e121d9153111f32fc16e93ae20ed18
jyh071116 <jyh071116@gmail.com>
2024-03-14 10:36:50 +0900

    💍 P5-14003

---
+++ 💍 P5/14003.py
@@ -0,0 +1,25 @@
+from bisect import bisect_left
+
+n = int(input())
+a = [0] + list(map(int, input().split()))
+d = [0]*(n+1)
+dp = [0]
+x = [-1000000001]
+
+for i in range(1, n+1):
+  if a[i] > x[-1]:
+    x.append(a[i])
+    d[i] = dp[-1]+1
+    dp.append(dp[-1]+1)
+  else:
+    idx = bisect_left(x, a[i])
+    d[i] = dp[idx]
+    x[idx] = a[i]
+
+print(dp[-1])
+lst = []
+for i in range(n, 0, -1):
+  if d[i] == dp[-1]:
+    lst.append(a[i])
+    dp.pop()
+print(*sorted(lst))
