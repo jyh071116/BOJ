f1c976a111fcf7337ab58c3959b3467bdf2beb9d
jyh071116 <jyh071116@gmail.com>
2024-03-22 08:24:54 +0900

    🪙 S1-11286

---
+++ 🪙 S1/11286.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+  a = int(input())
+  if a == 0:
+    if not arr:
+      print(0)
+    else:
+      print(heapq.heappop(arr)[1])
+  else:
+    heapq.heappush(arr, (abs(a), a))
